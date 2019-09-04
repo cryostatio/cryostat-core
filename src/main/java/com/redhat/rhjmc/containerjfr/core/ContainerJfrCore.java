c7552fe93f623c52212bfe0db4084311fe6b45c8
Andrew Azores <me@andrewazor.es>
2019-09-04 15:07:44 -0400

    Automatically set security manager system property

--- src/main/java/com/redhat/rhjmc/containerjfr/core/ContainerJfrCore.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/core/ContainerJfrCore.java
@@ -1,6 +1,9 @@
 package com.redhat.rhjmc.containerjfr.core;
 
+import java.util.logging.LogManager;
+
 import com.redhat.rhjmc.containerjfr.core.jmc.RegistryProvider;
+import com.redhat.rhjmc.containerjfr.core.jmc.SecurityManager;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.RegistryFactory;
@@ -9,6 +12,8 @@ public class ContainerJfrCore {
     private ContainerJfrCore() { }
 
     public static void initialize() throws CoreException {
+        System.setProperty("org.openjdk.jmc.common.security.manager", SecurityManager.class.getCanonicalName());
+        LogManager.getLogManager().reset();
         RegistryFactory.setDefaultRegistryProvider(new RegistryProvider());
     }
 }
