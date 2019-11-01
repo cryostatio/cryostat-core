6aee49083e67d33a2d79d999f42f288c65c3ca4c
Andrew Azores <me@andrewazor.es>
2019-11-01 17:45:29 -0400

    Improve handling of JMXServiceURLs

--- src/main/java/com/redhat/rhjmc/containerjfr/core/net/JFRConnection.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/core/net/JFRConnection.java
@@ -1,39 +1,32 @@
 package com.redhat.rhjmc.containerjfr.core.net;
 
-import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
 
 import com.redhat.rhjmc.containerjfr.core.sys.Clock;
 import com.redhat.rhjmc.containerjfr.core.tui.ClientWriter;
 
+import org.openjdk.jmc.rjmx.ConnectionToolkit;
+import org.openjdk.jmc.rjmx.IConnectionDescriptor;
 import org.openjdk.jmc.rjmx.IConnectionHandle;
 import org.openjdk.jmc.rjmx.IConnectionListener;
 import org.openjdk.jmc.rjmx.internal.DefaultConnectionHandle;
-import org.openjdk.jmc.rjmx.internal.JMXConnectionDescriptor;
 import org.openjdk.jmc.rjmx.internal.RJMXConnection;
 import org.openjdk.jmc.rjmx.internal.ServerDescriptor;
 import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
 import org.openjdk.jmc.rjmx.services.jfr.internal.FlightRecorderServiceFactory;
-import org.openjdk.jmc.ui.common.security.InMemoryCredentials;
 
 public class JFRConnection implements AutoCloseable {
 
-    static final String URL_FORMAT = "service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi";
     public static final int DEFAULT_PORT = 9091;
 
     private final ClientWriter cw;
-    private final JMXServiceURL url;
     private final RJMXConnection rjmxConnection;
     private final IConnectionHandle handle;
     private final IFlightRecorderService service;
 
-    JFRConnection(ClientWriter cw, String host, int port) throws Exception {
-        this(cw, new JMXServiceURL(String.format(URL_FORMAT, host, port)));
-    }
-
-    JFRConnection(ClientWriter cw, JMXServiceURL url) throws Exception {
+    JFRConnection(ClientWriter cw, IConnectionDescriptor cd) throws Exception {
         this.cw = cw;
-        this.url = url;
-        this.rjmxConnection = attemptConnect(url);
+        this.rjmxConnection = attemptConnect(cd);
         this.handle = new DefaultConnectionHandle(rjmxConnection, "RJMX Connection", new IConnectionListener[0]);
         this.service = new FlightRecorderServiceFactory().getServiceInstance(handle);
     }
@@ -47,15 +40,25 @@ public class JFRConnection implements AutoCloseable {
     }
 
     public long getApproximateServerTime(Clock clock) {
-        return rjmxConnection.getApproximateServerTime(clock.getWallTime());
+        return this.rjmxConnection.getApproximateServerTime(clock.getWallTime());
     }
 
     public String getHost() {
-        return this.url.getHost();
+        try {
+            return ConnectionToolkit.getHostName(this.rjmxConnection.getConnectionDescriptor().createJMXServiceURL());
+        } catch (IOException e) {
+            cw.println(e);
+            return "unknown";
+        }
     }
 
     public int getPort() {
-        return this.url.getPort();
+        try {
+            return ConnectionToolkit.getPort(this.rjmxConnection.getConnectionDescriptor().createJMXServiceURL());
+        } catch (IOException e) {
+            cw.println(e);
+            return 0;
+        }
     }
 
     public void disconnect() {
@@ -67,14 +70,9 @@ public class JFRConnection implements AutoCloseable {
         this.disconnect();
     }
 
-    private RJMXConnection attemptConnect(JMXServiceURL url) throws Exception {
-        JMXConnectionDescriptor cd = new JMXConnectionDescriptor(
-                url,
-                new InMemoryCredentials(null, null));
-        ServerDescriptor sd = new ServerDescriptor(null, "Container", null);
-
+    private RJMXConnection attemptConnect(IConnectionDescriptor cd) throws Exception {
         try {
-            RJMXConnection conn = new RJMXConnection(cd, sd, JFRConnection::failConnection);
+            RJMXConnection conn = new RJMXConnection(cd, new ServerDescriptor(), JFRConnection::failConnection);
             if (!conn.connect()) {
                 failConnection();
             }
--- src/main/java/com/redhat/rhjmc/containerjfr/core/net/JFRConnectionToolkit.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/core/net/JFRConnectionToolkit.java
@@ -4,6 +4,8 @@ import javax.management.remote.JMXServiceURL;
 
 import com.redhat.rhjmc.containerjfr.core.tui.ClientWriter;
 
+import org.openjdk.jmc.rjmx.ConnectionDescriptorBuilder;
+
 public class JFRConnectionToolkit {
 
     private final ClientWriter cw;
@@ -13,7 +15,7 @@ public class JFRConnectionToolkit {
     }
 
     public JFRConnection connect(JMXServiceURL url) throws Exception {
-        return new JFRConnection(cw, url);
+        return new JFRConnection(cw, new ConnectionDescriptorBuilder().url(url).build());
     }
 
     public JFRConnection connect(String host) throws Exception {
@@ -21,6 +23,6 @@ public class JFRConnectionToolkit {
     }
 
     public JFRConnection connect(String host, int port) throws Exception {
-        return new JFRConnection(cw, host, port);
+        return new JFRConnection(cw, new ConnectionDescriptorBuilder().hostName(host).port(port).build());
     }
 }
