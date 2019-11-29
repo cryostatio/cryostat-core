56e6f00179ae037f6df51d4e24fc026f6d52dddb
Andrew Azores <me@andrewazor.es>
2019-11-29 11:27:13 -0500

    Add now() method and deprecate getWallTime

--- src/main/java/com/redhat/rhjmc/containerjfr/core/sys/Clock.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/core/sys/Clock.java
@@ -1,15 +1,25 @@
 package com.redhat.rhjmc.containerjfr.core.sys;
 
+import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 public class Clock {
 
+    /**
+     * @deprecated
+     * Use #now()
+     */
+    @Deprecated
     public long getWallTime() {
         return System.currentTimeMillis();
     }
 
     public long getMonotonicTime() {
         return System.nanoTime();
+    }
+
+    public Instant now() {
+        return Instant.now();
     }
 
     public void sleep(int millis) throws InterruptedException {
