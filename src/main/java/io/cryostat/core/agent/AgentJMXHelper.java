ace15efcea626facd72eed76b3044b9fd584d05a
Joshua Matsuoka <Josh.matsuoka@gmail.com>
2021-11-12 18:25:38 -0500

    feat(agentplugin): add support for JMC agent plugin (#103)
    * Basic agent functionality (TODO: Method & field capturing xml model)
    
    * Added more classes for agent probe xml modelling, added support for deleting templates
    
    * Cleaning up localProbetemplateService, adding XML Validation
    
    * Cleanup, adding tests, implementing getTemplate
    
    * Remove extraneous non-nls tags, cleanup, rework probeTemplate.deserialize
    
    * Remove extraneous non-nls tags
    
    * clean up unnecessary catches, turn ProbeTemplateService into an interface, clean up getLocalTemplates
    
    * Renaming ProbeTemplateService
    
    * Adding invalid converter/relation key to exception message, rethrowing exceptions in AgentJMXHelper
    
    * running spotless

---
+++ src/main/java/io/cryostat/core/agent/AgentJMXHelper.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright The Cryostat Authors
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software (each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ * The above copyright notice and either this complete permission notice or at
+ * a minimum a reference to the UPL must be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.cryostat.core.agent;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+import org.openjdk.jmc.rjmx.ConnectionException;
+import org.openjdk.jmc.rjmx.IConnectionHandle;
+
+public class AgentJMXHelper {
+
+    private static final Logger logger = Logger.getLogger(AgentJMXHelper.class.getName());
+    private static final String AGENT_OBJECT_NAME =
+            "org.openjdk.jmc.jfr.agent:type=AgentController";
+    private static final String DEFINE_EVENT_PROBES = "defineEventProbes";
+    private static final String RETRIEVE_EVENT_PROBES = "retrieveEventProbes";
+    private static final String RETRIEVE_CURRENT_TRANSFORMS = "retrieveCurrentTransforms";
+
+    private final IConnectionHandle connectionHandle;
+    private final MBeanServerConnection mbsc;
+
+    public AgentJMXHelper(IConnectionHandle connectionHandle) throws ConnectionException {
+        this.connectionHandle = connectionHandle;
+        mbsc = connectionHandle.getServiceOrDummy(MBeanServerConnection.class);
+    }
+
+    public IConnectionHandle getConnectionHandle() {
+        return connectionHandle;
+    }
+
+    public MBeanServerConnection getMBeanServerConnection() {
+        return mbsc;
+    }
+
+    public boolean isMXBeanRegistered() throws Exception {
+        try {
+            return mbsc.isRegistered(new ObjectName(AGENT_OBJECT_NAME));
+        } catch (MalformedObjectNameException | IOException e) {
+            logger.log(Level.SEVERE, "Could not check if agent MXBean is registered", e);
+            throw e;
+        }
+    }
+
+    public String retrieveEventProbes() throws Exception {
+        try {
+            Object result =
+                    mbsc.invoke(
+                            new ObjectName(AGENT_OBJECT_NAME),
+                            RETRIEVE_EVENT_PROBES,
+                            new Object[0],
+                            new String[0]);
+
+            return result.toString();
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Could not retrieve event probes", e);
+            throw e;
+        }
+    }
+
+    public Object retrieveCurrentTransforms() throws Exception {
+        try {
+            Object result =
+                    mbsc.invoke(
+                            new ObjectName(AGENT_OBJECT_NAME),
+                            RETRIEVE_CURRENT_TRANSFORMS,
+                            new Object[0],
+                            new String[0]);
+            return result;
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Could not retrieve current transforms", e);
+            throw e;
+        }
+    }
+
+    public void defineEventProbes(String xmlDescription) throws Exception {
+        try {
+            Object[] params = {xmlDescription};
+            String[] signature = {String.class.getName()};
+            mbsc.invoke(new ObjectName(AGENT_OBJECT_NAME), DEFINE_EVENT_PROBES, params, signature);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Could not define event probes: " + xmlDescription, e);
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/io/cryostat/core/agent/LocalProbeTemplateService.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright The Cryostat Authors
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software (each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ * The above copyright notice and either this complete permission notice or at
+ * a minimum a reference to the UPL must be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.cryostat.core.agent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.cryostat.core.FlightRecorderException;
+import io.cryostat.core.sys.Environment;
+import io.cryostat.core.sys.FileSystem;
+
+import org.xml.sax.SAXException;
+
+public class LocalProbeTemplateService implements ProbeTemplateService {
+
+    public static final String TEMPLATE_PATH = "CRYOSTAT_PROBE_TEMPLATE_PATH";
+
+    private final FileSystem fs;
+    private final Environment env;
+
+    public LocalProbeTemplateService(FileSystem fs, Environment env) throws IOException {
+        this.fs = fs;
+        this.env = env;
+        // Sanity check
+        if (!env.hasEnv(TEMPLATE_PATH)) {
+            throw new IOException(
+                    String.format(
+                            "Probe template directory does not exist, must be set using environment variable %s",
+                            TEMPLATE_PATH));
+        }
+        // Sanity check the directory is set up correctly
+        Path probeTemplateDirectory = fs.pathOf(env.getEnv(TEMPLATE_PATH));
+        if (!fs.exists(probeTemplateDirectory)
+                || !fs.isDirectory(probeTemplateDirectory)
+                || !fs.isReadable(probeTemplateDirectory)
+                || !fs.isWritable(probeTemplateDirectory)) {
+            throw new IOException(
+                    String.format(
+                            "Probe template directory %s does not exist, is not a directory, or has incorrect permissions.",
+                            probeTemplateDirectory.toString()));
+        }
+    }
+
+    public void addTemplate(InputStream inputStream, String filename) throws Exception {
+        try (inputStream) {
+            ProbeTemplate template = new ProbeTemplate();
+            // If validation fails this will throw a ProbeValidationException with details
+            template.deserialize(inputStream);
+            Path path = fs.pathOf(env.getEnv(TEMPLATE_PATH), filename);
+            if (fs.exists(path)) {
+                throw new Exception(
+                        String.format(
+                                "Event template \"%s\" already exists", template.getFileName()));
+            }
+            fs.writeString(
+                    path,
+                    new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                            .lines()
+                            .collect(Collectors.joining("\n")));
+        }
+    }
+
+    public void deleteTemplate(String templateName) throws Exception {
+        if (!fs.deleteIfExists(fs.pathOf(env.getEnv(TEMPLATE_PATH), templateName))) {
+            throw new IOException(
+                    String.format("Probe template \"%s\" does not exist", templateName));
+        }
+    }
+
+    public String getTemplate(String templateName) throws Exception {
+        Path probeTemplatePath = fs.pathOf(env.getEnv(TEMPLATE_PATH), templateName);
+        ProbeTemplate template = new ProbeTemplate();
+        template.deserialize(fs.newInputStream(probeTemplatePath));
+        return template.serialize();
+    }
+
+    protected List<Path> getLocalTemplates() throws IOException {
+        String dirName = env.getEnv(TEMPLATE_PATH);
+        Path dir = fs.pathOf(dirName);
+        if (!fs.isDirectory(dir) || !fs.isReadable(dir)) {
+            throw new IOException(String.format("%s is not a readable directory", dirName));
+        }
+        return fs.listDirectoryChildren(dir).stream()
+                .map(dir::resolve)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ProbeTemplate> getTemplates() throws FlightRecorderException {
+        try {
+            List<ProbeTemplate> templates = new ArrayList<>();
+            for (Path path : getLocalTemplates()) {
+                try (InputStream stream = fs.newInputStream(path)) {
+                    ProbeTemplate template = new ProbeTemplate();
+                    template.deserialize(stream);
+                    templates.add(template);
+                }
+            }
+            return templates;
+        } catch (IOException | SAXException e) {
+            throw new FlightRecorderException(e);
+        }
+    }
+}
