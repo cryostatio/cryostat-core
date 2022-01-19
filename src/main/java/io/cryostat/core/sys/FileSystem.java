ec1b437769ea593e2861a708ce910881d0ab75a0
Andrew Azores <aazores@redhat.com>
2022-01-19 17:11:37 +0000

    feat(filesystem): add abstractions for creating (temp) files/dirs (#54)
    * Add abstractions for creating (temp) files/dirs
    
    * apply spotless formatting
    
    * correct typo

--- src/main/java/io/cryostat/core/sys/FileSystem.java
+++ src/main/java/io/cryostat/core/sys/FileSystem.java
@@ -45,6 +45,7 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -109,5 +110,36 @@ public class FileSystem {
 
     public long size(Path path) throws IOException {
         return Files.size(path);
+    }
+
+    public Path createFile(Path path, FileAttribute<?>... attrs) throws IOException {
+        return Files.createFile(path, attrs);
+    }
+
+    public Path createTempFile(String prefix, String suffix, FileAttribute<?>... attrs)
+            throws IOException {
+        return Files.createTempFile(prefix, suffix, attrs);
+    }
+
+    public Path createTempFile(Path path, String prefix, String suffix, FileAttribute<?>... attrs)
+            throws IOException {
+        return Files.createTempFile(path, prefix, suffix, attrs);
+    }
+
+    public Path createDirectory(Path path, FileAttribute<?>... attrs) throws IOException {
+        return Files.createDirectory(path, attrs);
+    }
+
+    public Path createDirectories(Path path, FileAttribute<?>... attrs) throws IOException {
+        return Files.createDirectories(path, attrs);
+    }
+
+    public Path createTempDirectory(String prefix, FileAttribute<?>... attrs) throws IOException {
+        return Files.createTempDirectory(prefix, attrs);
+    }
+
+    public Path createTempDirectory(Path path, String prefix, FileAttribute<?>... attrs)
+            throws IOException {
+        return Files.createTempDirectory(path, prefix, attrs);
     }
 }
