d43c0822f4707bb5a2bb53378704497610302d0a
Elliott Baron <ebaron@redhat.com>
2024-05-14 18:10:41 -0400

    ci(maven): use script for releasing snapshots (#397)

---
+++ release-snapshot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "📦 Staging artifacts..."
+./mvnw --batch-mode --no-transfer-progress -Ppublication -DskipTests=true -Dskip.spotless=true
+
+echo "🚀 Releasing..."
+./mvnw --batch-mode --no-transfer-progress -Prelease jreleaser:deploy
+
+echo "🎉 Done!"
