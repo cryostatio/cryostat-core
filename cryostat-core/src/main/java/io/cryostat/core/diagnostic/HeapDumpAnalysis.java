f467cbef87ee005a32d0ee1fa49f601f7c40d84e
Andrew Azores <aazores@ibm.com>
2026-08-31 13:21:59 -0400

    spotbugs

--- cryostat-core/src/main/java/io/cryostat/core/diagnostic/HeapDumpAnalysis.java
+++ cryostat-core/src/main/java/io/cryostat/core/diagnostic/HeapDumpAnalysis.java
@@ -106,9 +106,6 @@ public class HeapDumpAnalysis {
         weakHashMaps = new ArrayList<WeakHashMapEntry>();
     }
 
-    @SuppressFBWarnings(
-            value = "NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD",
-            justification = "ObjectToIntMap.Entry is initialized in the JOverflow code")
     public void analyze(Path file)
             throws IOException, DumpCorruptedException, HprofParsingCancelledException {
         VerboseOutputCollector vc = new VerboseOutputCollector();
